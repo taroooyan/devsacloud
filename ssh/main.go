6a630bfb06c301b34123b8fc821ac2165e217531
taroooyan <taroooyan@gmail.com>
2017-02-20 00:18:13 +0900

    WIP ssh option.

---
+++ ssh/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	if len(os.Args) != 4 {
+		log.Fatalf("Usage: %s <user> <host:port> <command>", os.Args[0])
+	}
+
+	client, session, err := connectToHost(os.Args[1], os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	// out, err := session.CombinedOutput(os.Args[3])
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	modes := ssh.TerminalModes{
+		// ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		session.Close()
+    return
+  }
+
+  session.Stdout = os.Stdout
+  session.Stderr = os.Stderr
+  session.Stdin = os.Stdin
+  session.Run("bash")
+
+	defer client.Close()
+}
+
+func connectToHost(user, host string) (*ssh.Client, *ssh.Session, error) {
+	var pass string
+	fmt.Print("Password: ")
+	fmt.Scanf("%s\n", &pass)
+
+	sshConfig := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+      ssh.Password(pass),
+    },
+	}
+
+	client, err := ssh.Dial("tcp", host, sshConfig)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		client.Close()
+		return nil, nil, err
+	}
+
+	return client, session, nil
+}
